8818564890bd40f0bd343c6e0c5b7bfde9e68171
Alex Galani <alex@cloudcitizen.eu>
2022-09-14 10:30:39 +0200

    demo commit

--- terraform/github.tf
+++ terraform/github.tf
@@ -5,3 +5,21 @@ resource "aws_iam_openid_connect_provider" "github" {
   ]
   thumbprint_list = ["6938fd4d98bab03faadb97b34396831e3780aea1"]
 }
+
+resource "aws_iam_role" "demo" {
+  name = "demo"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Sid    = ""
+        Principal = {
+          Service = "ec2.amazonaws.com"
+        }
+      },
+    ]
+  })
+}
